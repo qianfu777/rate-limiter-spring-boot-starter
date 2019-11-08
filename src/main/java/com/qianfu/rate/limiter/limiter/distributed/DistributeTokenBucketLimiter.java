46924acfc3543f0039966b7b9c94a8d98e845a33
qianfu <qianfu_53042@wdai.com>
2019-11-08 10:20:45 +0800

    fix bug

---
+++ src/main/java/com/qianfu/rate/limiter/limiter/distributed/DistributeTokenBucketLimiter.java
@@ -0,0 +1,31 @@
+package com.qianfu.rate.limiter.limiter.distributed;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.scripting.support.ResourceScriptSource;
+
+import java.util.Collections;
+
+/**
+ * 基于令牌桶实现的分布式限流器
+ *
+ * @author qianfu
+ * @date 2019/11/5
+ */
+public class DistributeTokenBucketLimiter extends AbstractDistributedLimiter {
+    private DefaultRedisScript<Long> script;
+
+
+    public DistributeTokenBucketLimiter(String key, int limit, StringRedisTemplate redisTemplate) {
+        super(key, limit, redisTemplate);
+        script = new DefaultRedisScript<>();
+        script.setResultType(Long.class);
+        script.setScriptSource(new ResourceScriptSource(new ClassPathResource("lua/token_bucket.lua")));
+    }
+
+    @Override
+    public boolean tryAcquire() {
+        return redisTemplate.execute(script, Collections.singletonList(key)) <= limit;
+    }
+}
--- src/main/java/com/qianfu/rate/limiter/limiter/local/LocalLeakyBucketLimiter.java
+++ src/main/java/com/qianfu/rate/limiter/limiter/local/LocalLeakyBucketLimiter.java
@@ -1,8 +1,5 @@
 package com.qianfu.rate.limiter.limiter.local;
 
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
-
 /**
  * 基于漏桶实现的本地限流器
  *
@@ -13,34 +10,33 @@ public class LocalLeakyBucketLimiter extends AbstractLocalLimiter {
 
     private int rate;
     private int capacity;
-    private AtomicInteger water;
-    private AtomicLong refreshTime;
+    private int water;
+    private long refreshTime;
 
     public LocalLeakyBucketLimiter(String key, int limit) {
         super(key, limit);
         rate = 1;
         capacity = limit;
-        water = new AtomicInteger(0);
-        refreshTime = new AtomicLong(System.currentTimeMillis());
+        water = 0;
+        refreshTime = System.currentTimeMillis();
     }
 
     @Override
     public boolean tryAcquire() {
-        refreshWater();
-        if (water.get() < capacity) {
-            water.incrementAndGet();
-            return true;
+        synchronized (lock) {
+            refreshWater();
+            if (water < capacity) {
+                water++;
+                return true;
+            }
+            return false;
         }
-
-        return false;
     }
 
     private void refreshWater() {
-        int tempWatch = water.get();
-        long tempTime = refreshTime.get();
         long now = System.currentTimeMillis();
         // 水随着时间流逝,不断流走,最多就流干到0.
-        water.compareAndSet(tempWatch, (int) Math.max(0, tempWatch - (now - tempTime) / 1000 * rate));
-        refreshTime.compareAndSet(tempTime, now);
+        water = (int) Math.max(0, water - (now - refreshTime) / 1000 * rate);
+        refreshTime = now;
     }
 }
